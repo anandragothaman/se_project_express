a681e8924f69fd192657ed71b104b606315cffe0
Anand Ragothaman <r.anandragothaman@gmail.com>
2025-08-20 01:55:19 -0500

    minor fix

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ mongoose
   .catch(console.error);
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
-  };
-  next();
-});
 app.use("/", mainRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
